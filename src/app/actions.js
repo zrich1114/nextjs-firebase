530888258a0584c85ba7076847a014d498650605
Zach Richardson <zachrichardson1114@gmail.com>
2024-12-28 18:06:02 -0500

    Allow users to submit restaurant reviews

--- src/app/actions.js
+++ src/app/actions.js
@@ -4,7 +4,18 @@ import { addReviewToRestaurant } from "@/src/lib/firebase/firestore.js";
 import { getAuthenticatedAppForUser } from "@/src/lib/firebase/serverApp.js";
 import { getFirestore } from "firebase/firestore";
 
-// This is a Server Action
+// This is a next.js server action, which is an alpha feature, so
+// use with caution.
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
-// Replace the function below
-export async function handleReviewFormSubmission(data) {}
+export async function handleReviewFormSubmission(data) {
+  const { app } = await getAuthenticatedAppForUser();
+  const db = getFirestore(app);
+
+  await addReviewToRestaurant(db, data.get("restaurantId"), {
+    text: data.get("text"),
+    rating: data.get("rating"),
+
+    // This came from a hidden form field.
+    userId: data.get("userId"),
+  });
+}
